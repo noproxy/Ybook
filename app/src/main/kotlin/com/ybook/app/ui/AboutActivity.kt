8d39af292dcf63620287abff85228808aa0abc6e
Carlos <toxz@foxmail.com>
2014-12-21 14:53:32 +0800

    update aboutView's contain.

--- app/src/main/kotlin/com/ybook/app/ui/AboutActivity.kt
+++ app/src/main/kotlin/com/ybook/app/ui/AboutActivity.kt
@@ -4,15 +4,16 @@ import android.app.Activity
 import android.os.Bundle
 import com.ybook.app.R
 import android.support.v4.app.FragmentActivity
+import com.ybook.app.swipebacklayout.SwipeBackActivity
 
 /**
  * Created by Carlos on 2014/12/21.
  */
-public class AboutActivity : FragmentActivity() {
+public class AboutActivity : SwipeBackActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        super<FragmentActivity>.onCreate(savedInstanceState)
+        super<SwipeBackActivity>.onCreate(savedInstanceState)
         setContentView(R.layout.activity_about)
-        getSupportFragmentManager().beginTransaction().replace(R.id.container, AboutFragment())
+        getSupportFragmentManager().beginTransaction().replace(R.id.container, AboutFragment()).commit()
         getActionBar() setTitle (getString(R.string.action_about))
     }
 }
